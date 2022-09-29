7157b6b3302c237f06d1fd49598bf1dc36ebc831
AyushR1 <ayushrathore12501@gmail.com>
2022-09-30 01:01:12 +0530

    Add Nightlight shortcut script for Ubuntu.
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

---
+++ nl.sh
@@ -0,0 +1,1 @@
+bash -c "if [[ $(gsettings get org.gnome.settings-daemon.plugins.color night-light-enabled) == "true" ]]; then gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled false; else gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true; fi"
